c7ddbeffb73d0f921dc5d5bf783eeb58b3d8cdc3
JuHee Kim <rlawngml59@gmail.com>
2022-09-24 15:39:16 +0900

    start_to_lear_HTML.html
    - create a file.

--- JuHeedefinedError.py
+++
@@ -1,5 +0,0 @@
-class NoNegativeNumberError(Exception):
-
-    def __init__(self, abc):
-
-        super().__init__('User defined Error by oochoo  : ' + abc)
--- enter the website using "with".py
+++
@@ -1,5 +0,0 @@
-import urllib.request
-
-with urllib.request.urlopen('http://python.org/') as response:
-    html = response.read(1000)
-    print(html)
