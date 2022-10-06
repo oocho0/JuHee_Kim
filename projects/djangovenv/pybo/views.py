5f1f35abf57a17681b50204b3ca27c0a61481910
JuHee Kim <rlawngml59@gmail.com>
2022-10-07 00:05:14 +0900

    django project
    - make Question/Answer site

--- projects/djangovenv/pybo/views.py
+++ projects/djangovenv/pybo/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
+from django.utils import timezone
+
 
 def index(request):
     """pybo 목록 출력"""
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list' : question_list}
+    context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
+
 
 def detail(request, question_id):
     """pybo 내용 출력"""
-    question = Question.objects.get(id=question_id)
-    context = {'question' : question}
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    """pybo 답변 등록"""
+
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
+#    question = get_object_or_404(Question, pk=question_id)
+#   answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+#   answer.save()
