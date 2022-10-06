716ff25907d35fc070fe68794cd1f3a97b34e8ac
JuHee Kim <rlawngml59@gmail.com>
2022-10-06 14:11:44 +0900

    start to learn the django!

--- django/oochoodjango/oochoodjango/settings.py
+++ django/oochoodjango/oochoodjango/settings.py
@@ -54,13 +54,13 @@ ROOT_URLCONF = 'oochoodjango.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'BACKEND': 'django.templates.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.templates.context_processors.debug',
+                'django.templates.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
--- django/oochoodjango/pybo/views.py
+++ django/oochoodjango/pybo/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-        return HttpResponse('Welcome to django world by oochoo')
+        """pybo 목록 출력"""
+        question_list = Question.objects.order_by('-create_date')
+        context = {'question_list' : question_list}
+        return render(request, 'pybo/question_list.html', context)
