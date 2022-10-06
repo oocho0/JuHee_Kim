f7047f15d07c829888efd0c937646244671aa914
JuHee Kim <rlawngml59@gmail.com>
2022-10-06 21:35:31 +0900

    django project
    - setting the django admin site

--- projects/djangovenv/pybo/models.py
+++ projects/djangovenv/pybo/models.py
@@ -5,6 +5,9 @@ class Question(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.subject
+
 class Answer(models.Model) :
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
