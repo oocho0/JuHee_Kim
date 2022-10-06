edf72294c28ae53685e100c1ced5d1a5062a61d2
JuHee Kim <rlawngml59@gmail.com>
2022-10-06 22:15:24 +0900

    django project
    - make /pybo/ site
    - make the site when you click the 'question' hyperlink

--- projects/djangovenv/pybo/urls.py
+++ projects/djangovenv/pybo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:question_id>/', views.detail),
 ]
--- projects/djangovenv/pybo/views.py
+++ projects/djangovenv/pybo/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, Welcome to django world by oochoo")
+    """pybo 목록 출력"""
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list' : question_list}
+    return render(request, 'pybo/question_list.html', context)
 
+def detail(request, question_id):
+    """pybo 내용 출력"""
+    question = Question.objects.get(id=question_id)
+    context = {'question' : question}
+    return render(request, 'pybo/question_detail.html', context)
