15157a26d7d6bff90e1e4ec75d5e4e16fe41d21f
Juhee Kim <>
2022-11-20 20:22:58 +0900

    let's start to learn "Pandas"

--- Python_Anaconda/PandasAPI/series func.py
+++ Python_Anaconda/PandasAPI/series func.py
@@ -28,34 +28,3 @@ print("using index_name")
 print(data_series3['nickname'], "\n")
 print(data_series3[['date', 'have_car']], "\n")
 print(data_series3['date': 'have_car'])
-
-# DataFrame() 함수로 데이터프레임 변환. 변수 df에 저장
-exam_data = {'수학' : [ 90, 80, 70], '영어' : [ 98, 89, 95],
-             '음악' : [ 85, 95, 100], '체육' : [ 100, 90, 90]}
-
-df = pd.DataFrame(exam_data, index=['서준', '우현', '인아'])
-print(df)       # 데이터프레임 출력
-print('\n')
-
-# 행 인덱스를 사용하여 행 1개를 선택
-label1 = df.loc['서준']    # loc 인덱서 활용
-position1 = df.iloc[0]     # iloc 인덱서 활용
-print(label1)
-print('\n')
-print(position1)
-print('\n')
-
-# 행 인덱스를 사용하여 2개 이상의 행 선택
-label2 = df.loc[['서준', '우현']]
-position2 = df.iloc[[0, 1]]
-print(label2)
-print('\n')
-print(position2)
-print('\n')
-
-# 행 인덱스의 범위를 지정하여 행 선택
-label3 = df.loc['서준':'우현']
-position3 = df.iloc[0:1]
-print(label3)
-print('\n')
-print(position3)
---
+++ Python_Anaconda/PandasAPI/using_index.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+family_data = {'oochoo': [93, 11, 18], 'mydong': [91, 5, 27], 'dalgom': [22, 1, 15], 'puleum': [21, 1, 4]}
+df = pd.DataFrame(family_data, index=['year', 'month', 'date'])
+print(df, '\n', "*"*80)
+
+print("="*80, "\n row index to set_index()")
+df_1 = df.copy()
+df_1 = df_1.set_index(["oochoo"])
+print(df_1, '\n', "*"*80)
+df_2 = df.copy()
+df_2 = df_2.set_index("oochoo")
+print(df_2, '\n', "*"*80)
+df_3 = df.copy()
+df_3 = df_3.set_index(["dalgom", "puleum"])
+print(df_3)
+
+print("="*80, "\n row index to reindex()")
+data_dic = {'num0': [2, 4, 6], 'num1': [8, 10, 12], 'num2': [14, 16, 18], 'num3': [20, 22, 24], 'num4': [26, 28, 30]}
+df = pd.DataFrame(data_dic, index=['row_0', 'row_1', 'row_2'])
+print(df, '\n', "*"*80)
+
+df_4 = df.copy()
+df_4 = df_4.reindex(['row_0', 'row_1', 'row_2', 'row_3', 'row_4'])
+print(df_4, '\n', "*"*80)
+
+df_5 = df.copy()
+df_5 = df_5.reindex(['row_0', 'row_1', 'row_2', 'row_3', 'row_4'], fill_value=0)
+print(df_5)
+
+print("="*80, "\n reset row index")
+df_6 = df.copy()
+df_6 = df_6.reset_index()
+print(df_6)
+
+print("="*80, "\n sort by row index")
+df_7 = df.copy()
+df_7 = df_7.sort_index(ascending=False)
+print(df_7)
