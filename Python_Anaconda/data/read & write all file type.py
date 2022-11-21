a16e5597aedcace14103363512ad48e2dfe7ede6
Juhee Kim <>
2022-11-21 10:38:33 +0900

    1. Basic Knowledge of Pandas 2. input & output data from dataframe

---
+++ Python_Anaconda/data/read & write all file type.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+print("read csv file")
+df_1 = pd.read_csv('read_csv_sample.csv')
+print(df_1, "\n", "*"*80)
+df_2 = pd.read_csv('read_csv_sample.csv', header=None)
+print(df_2, "\n", "*"*80)
+df_3 = pd.read_csv('read_csv_sample.csv', index_col=None)
+print(df_3, "\n", "*"*80)
+df_4 = pd.read_csv('read_csv_sample.csv', index_col='c0')
+print(df_4)
+
---
+++ Python_Anaconda/series func.py
@@ -0,0 +1,30 @@
+﻿import pandas as pd
+
+print("dictionary to series")
+dic_kv = {"oochoo": 30, "mydong": 32, "dalgom": 1}
+data_series1 = pd.Series(dic_kv)
+print(type(data_series1), "\n")
+print(data_series1, "\n\n", "="*70)
+
+print("list to series")
+data_list = ['Dalgom', 3, '2022-11-17', False, 5.18]
+data_series2 = pd.Series(data_list)
+print(data_series2, "\n")
+se_idx = data_series2.index
+se_val = data_series2.values
+print(se_idx, ":", se_val, "\n\n", "="*70)
+
+print("tuple to series")
+data_tuple = ('oochoo', '2022-11-17', 'Soup', False)
+data_series3 = pd.Series(data_tuple, index=['nickname', 'date', 'like_food', 'have_car'])
+print(data_series3, "\n\n", "="*70)
+
+print("indexing")
+print(data_series3[0], "\n")
+print(data_series3[[1, 3]], "\n")
+print(data_series3[1: 3], "\n\n", "="*70)
+
+print("using index_name")
+print(data_series3['nickname'], "\n")
+print(data_series3[['date', 'have_car']], "\n")
+print(data_series3['date': 'have_car'])
